e22f707ea1eded37cfaa936c02dfd4f62ad0bdc4
@andrus.81 <andrus.81@hotmail.com>
2020-01-21 06:27:23 -0500

    The functionality of adding task, adding user, adding a task to a user was added, the delete icon was added

--- js/index.js
+++ js/index.js
@@ -11,6 +11,17 @@ new Vue({
             name:'',
             description:''
         },
+        nameTask:'', 
+        descriptionTask:'',
+        userIdTask:'',
+        username:'',
+       // result: '',
+        inputUser:{
+            name:'',
+            description:'',
+            selectUser:'',
+            selectTask:''
+        },
         listAllTask:[],
         listTask:[],
         listTaskOpen:[],
@@ -46,16 +57,90 @@ new Vue({
             });  
             this.listTask.push({name:this.inputTask.name,description:this.inputTask.description});
 
+        },
+        DeleteTask:function (id) {
+            swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover this task!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              })
+              .then((willDelete) => {
+
+            axios.delete(`${Api}Task/${id}`,).then(response => {
+                this.removeTaskFromArray(id);    
+              });
+              });
+            },
+        removeTaskFromArray: function(id){
+            for(let i = 0;i<this.listTask.length;i++){
+                if(this.listTask[i]._id == id){
+                    this.listTask.splice(i, 1);
+                    return;
+                }
+            }
+
+            for(let i = 0;i<this.listTaskOpen.length;i++){
+                if(this.listTaskOpen[i]._id == id){
+                    this.listTaskOpen.splice(i, 1);
+                    return;
+                }
+            }
+
+            for(let i = 0;i<this.listTaskProgress.length;i++){
+                if(this.listTaskProgress[i]._id == id){
+                    this.listTaskProgress.splice(i, 1);
+                    return;
+                }
+            }
+            for(let i = 0;i<this.listTaskCompleted.length;i++){
+                if(this.listTaskCompleted[i]._id == id){
+                    this.listTaskCompleted.splice(i, 1);
+                    return;
+                }
+            }
+
+            for(let i = 0;i<this.listTaskArchived.length;i++){
+                if(this.listTaskArchived[i]._id == id){
+                    this.listTaskArchived.splice(i, 1);
+                    return;
+                }
+            }
+
+            
+        },
+        AddUser: function(){
+            axios.post(Api+"User",{name:this.inputUser.name,description:this.inputUser.description}).then(response =>{
+                swal("User", response.data, "success");
+            }).catch(err=>{
+                swal("Error creating task", err.response, "error");
+            });  
+            this.listUser.push({name:this.inputUser.name,description:this.inputUser.description});
+
+        },
+        AssingUserTask: function(){           
+            axios.put(Api+"Task/"+this.inputUser.selectTask,{userid:this.inputUser.selectUser}).then(response =>{
+                swal("task", response.data, "success");
+            }).catch(err=>{
+                swal("Error assign task", err.response.data.Data[0], "error");
+            });
+        },
+        showInfo: function(id){
+            axios.get(Api+"Task?id="+id).then(response =>{
+                this.nameTask=  response.data[0].name; 
+                this.descriptionTask=  response.data[0].name; 
+                this.userIdTask=  response.data[0].name; 
+            }); 
+
+            axios.get(Api+"User?id="+this.userIdTask).then(response =>{
+                this.username=  response.data[0].name;                 
+            }); 
+
         }
+
 
     }
    
 });
 
-/*   GetListUser: function(){
-            axios.get(Api+"Task").then(response =>{
-                this.listUser=  response.data; 
-
-            }); 
-        }
-*/ 
